d9cfcb563365c0dfa67595ea9d50614b857ad1c9
TanyaZvereva <darkmarketgirl3@yandex.ru>
2022-06-27 14:20:59 +0300

    commit 27.06

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HighLight from './components/highlight';
 import Time from './components/time';
 
 function App() {
   return (
     <div className="App">
      <Time/>
+     <HighLight/>
     </div>
   );
 }
